a38196740310fb91ddc4bcbc191bef7bab4de210
fespiritu <ferdinandespiritu@hotmail.com>
2017-09-30 18:24:15 -0700

    Epson project
    Initial commit

---
+++ EpsonRepoSolution/Epson.MvcWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Epson.MvcWeb.MvcApplication" Language="C#" %>
---
+++ EpsonRepoSolution/Epson.MvcWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Epson.MvcWeb.Startup))]
+namespace Epson.MvcWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
